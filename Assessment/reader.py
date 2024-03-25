ed200b3185d66b04357b5e25e41cf051f948e821
Valerio <valerio.balsamini@gmail.com>
2024-03-25 19:02:32 +0000

    Lesson and Assessment done

--- Assessment/reader.py
+++ Assessment/reader.py
@@ -1,30 +1,59 @@
+# Importing Modules
 import sys
 import csv
 
-
-
-if len(sys.argv) < 6:
+# Check if enough arguments are provided
+if len(sys.argv) < 3:
     print("Usage: python3 reader.py source_file destination_file change1 change2 change3 change4")
     sys.exit(1)
 
-
+# Get the source_file, destination_file, and changes from CMD
 source_file = sys.argv[1]
-# destination_file = sys.argv[2]
-# change1 = sys.argv[3]
-# change2 = sys.argv[4]
-# change3 = sys.argv[5]
-# change4 = sys.argv[6]
+destination_file = sys.argv[2]
+changes = sys.argv[3:]
+
+# Check if changes are provided
+if not changes:
+    print("No changes provided!")
+    sys.exit(1)
 
 
-if source_file == "in.csv":
-    with open("in.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            print(row)
+try:
+    # Open the source CSV file for reading
+    with open(source_file, "r") as file:
+        # Read the CSV file and convert it into a list of lists
+        reader = list(csv.reader(file))
+        print("\nOriginal CSV content:\n")
+        for content_before in reader:
+            print(",".join(content_before))
+except FileNotFoundError:
+    print(f"\nFile not found: {file}")
+    sys.exit(1)
 
-# elif destination_file == "out.csv":
-#     # WRITE to file here
-#     print("lalalsssss")
+try:
+    for change in changes:
+        # Split each change into its components: column, row, and value
+        column, row, value = map(str.strip, change.split(","))
 
-# else:
-#     print("Invalid command!")
+        # Convert column and row to integers
+        column = int(column) - 1
+        row = int(row) - 1
+
+        # Update the value in the specified cell of the CSV data
+        reader[row][column] = value
+
+except ValueError or IndexError:
+    print("\nplease retry!")
+    sys.exit(1)
+
+try:
+    with open(destination_file, "w", newline="") as file2:
+        writer = csv.writer(file2)
+        writer.writerows(reader)
+except IOError:
+    print(f"\nError writing in file: {destination_file}")
+
+print("\nModified CSV Content:\n")
+for content_modified in reader:
+    print(",".join(content_modified))
+print("")
